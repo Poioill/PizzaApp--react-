ac791829d7ea1576c3c42b41cb1e2aa16dddda36
Poioill <satkynovaz@gmail.com>
2023-07-16 02:57:22 +0600

    main page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import BannerImage from '../assets/pizza.jpeg'
+import '../styles/Home.css';
 
 function Home() {
   return (
-    <div>Hello Home</div>
+    <div className='home' style={{backgroundImage: `url(${BannerImage})`}}>
+        <div className='_container'>
+            <div className='home__container'>
+                <h1> Perdo's Pizzeria</h1>
+                <p>PIZZA TO FIT ANY TASTE</p>
+                <Link to="/menu">
+                    <button>Order now!</button>
+                </Link> 
+            </div>
+        </div>
+    </div>
   )
 }
 
