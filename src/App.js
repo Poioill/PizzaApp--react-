ecfb10f46ab026ebf1f94bced016b0d428aebed0
Poioill <satkynovaz@gmail.com>
2023-07-15 22:23:40 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">LALALA</div>
+    <div className="App">        
+      <Router>
+        <Navbar/>
+        <Routes>
+            <Route exact path="/"  element={<Home/>} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import Logo from '../assets/pizzaLogo.png';
+import { Link } from 'react-router-dom';
+import "../styles/Navbar.css";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+        <div className='navbar__wrapper _container'>
+            <div className='leftSide'>
+                <img src={Logo}></img>
+            </div>
+            <div className='rightSide'>
+                <Link to="/">Home</Link>
+                <Link to="/menu">Menu</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+            </div>            
+        </div>
+    </div>
   )
 }
 
